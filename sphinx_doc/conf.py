d9ff012a3a1b1990ca2a334867b826beab565f95
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2024-12-02 16:47:52 +0100

    add options in conf.py

--- sphinx_doc/conf.py
+++ sphinx_doc/conf.py
@@ -10,6 +10,7 @@ project = 'Eqasim'
 copyright = '2024, Eqasim contributors'
 author = 'Eqasim contributors'
 release = 'v1.2.0'
+version = release[1:]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,5 +25,25 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_baseurl = "https://eqasim.readthedocs.io/stable/"
+html_theme_options = {
+    "collapse_navigation": True,
+    "navigation_depth": 2,
+    "show_prev_next": False,
+    "icon_links": [
+        {"name": "Home Page", "url": html_baseurl, "icon": "fas fa-home"},
+        {
+            "name": "GitHub",
+            "url": "https://github.com/eqasim-org/ile-de-france",
+            "icon": "fab fa-github-square",
+        },
+    ],
+    "navbar_end": ["theme-switcher", "navbar-icon-links"],
+    "secondary_sidebar_items": ["page-toc", "edit-this-page"],
+    "header_links_before_dropdown": 7,
+}
+
 html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+
+html_show_sourcelink = False
