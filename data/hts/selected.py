8359cc5db4a6c7e0f9a5f58815c7bd2da2681a0f
Marie Laurent <mlaurent@tellae.fr>
2024-12-11 17:30:42 +0100

    feat : filter & use hts

--- data/hts/selected.py
+++ data/hts/selected.py
@@ -12,6 +12,8 @@ def configure(context):
         context.stage("data.hts.edgt_lyon.reweighted", alias = "hts")
     elif hts == "edgt_44":
         context.stage("data.hts.edgt_44.reweighted", alias = "hts")
+    elif hts == "emp":
+        context.stage("data.hts.emp.reweighted", alias = "hts")
     else:
         raise RuntimeError("Unknown HTS: %s" % hts)
 
